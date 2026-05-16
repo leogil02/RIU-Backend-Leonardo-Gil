b4eee8d2507cdfd92b5445de33d141f9cb975bf0
Leonardo Gil <leonardo_gil02@hotmail.com>
2026-05-16 00:30:48 -0300

    feat: creación de GlobalMessages para no repetir strings similares constantemente

---
+++ src/main/java/com/leonardo/hotel_search_challenge/domain/shared/GlobalMessages.java
@@ -0,0 +1,12 @@
+package com.leonardo.hotel_search_challenge.domain.shared;
+
+public class GlobalMessages {
+
+    private GlobalMessages(){}
+
+    public static final String NULL_FIELD_MESSAGE_ERROR = "El campo %s no puede ser nulo";
+    public static final String BLANK_OR_EMPTY_FIELD_MESSAGE_ERROR = "El campo %s no puede estar vacío";
+    public static final String NEGATIVE_FIELD_MESSAGE_ERROR = "El campo %s no puede ser negativo";
+    public static final String CHECK_IN_BEFORE_CHECK_OUT_MESSAGE_ERROR = "El checkIn debe ser anterior al checkOut";
+
+}
