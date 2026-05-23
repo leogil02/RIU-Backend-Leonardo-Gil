58d74db8346d516384724c91fa053ac948887a1c
Leonardo Gil <leonardo_gil02@hotmail.com>
2026-05-23 01:39:51 -0300

    feat: agrego DLT en el consumer de Kafka

--- src/main/java/com/leonardo/hotel_search_challenge/infrastructure/adapter/in/messaging/HotelSearchedEventConsumer.java
+++ src/main/java/com/leonardo/hotel_search_challenge/infrastructure/adapter/in/messaging/HotelSearchedEventConsumer.java
@@ -6,7 +6,10 @@ import com.leonardo.hotel_search_challenge.infrastructure.config.KafkaTopics;
 import com.leonardo.hotel_search_challenge.infrastructure.adapter.in.messaging.mapper.HotelSearchedEventMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.BackOff;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,9 +26,19 @@ public class HotelSearchedEventConsumer {
             topics = KafkaTopics.HOTEL_AVAILABILITY_SEARCHES,
             groupId = "${spring.kafka.consumer.group-id}"
     )
+    @RetryableTopic(
+            attempts = "5",
+            backOff = @BackOff(delay = 1000, multiplier = 1.5),
+            dltTopicSuffix = ".dlt"
+    )
     public void handleHotelSearchedEvent(HotelSearchedEvent event){
         log.info("Evento recibido en el tópico {} con searchId={}",KafkaTopics.HOTEL_AVAILABILITY_SEARCHES, event.searchId());
         saveSearchUseCase.save(HotelSearchedEventMapper.toDomainEntity(event));
+    }
+
+    @DltHandler
+    public void handleDlt(HotelSearchedEvent event){
+        log.error("Evento enviado a DLT por alcanzar el numero máximo de reintentos: searchId = {}",event.searchId());
     }
 
 }
