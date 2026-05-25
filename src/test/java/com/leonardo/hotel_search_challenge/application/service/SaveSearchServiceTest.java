d4c60d24adf1ee4bd60377bbc9d91fcb32cbc5d7
Leonardo Gil <leonardo_gil02@hotmail.com>
2026-05-25 18:38:05 -0300

    test: cubro idempotencia en SaveSearchServiceTest y reformulo el happy path de este

--- src/test/java/com/leonardo/hotel_search_challenge/application/service/SaveSearchServiceTest.java
+++ src/test/java/com/leonardo/hotel_search_challenge/application/service/SaveSearchServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static com.leonardo.hotel_search_challenge.TestData.PERSISTED_HOTEL_SEARCH;
+import static com.leonardo.hotel_search_challenge.TestData.SEARCH_ID;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,10 +22,26 @@ class SaveSearchServiceTest {
     private SaveSearchService service;
 
     @Test
-    @DisplayName("Debe llamar al repositorio para guardar la búsqueda")
+    @DisplayName("Debe llamar al repositorio para guardar la búsqueda cuando la búsqueda no está persistida")
     void should_call_repository_to_save_search(){
+        when(repository.existsById(SEARCH_ID)).thenReturn(false);
+
         service.save(PERSISTED_HOTEL_SEARCH);
+
+        verify(repository).existsById(SEARCH_ID);
         verify(repository).save(PERSISTED_HOTEL_SEARCH);
+    }
+
+
+    @Test
+    @DisplayName("No debe persistir la búsqueda si esta ya se encuentra persistida")
+    void should_not_persist_when_search_exists(){
+        when(repository.existsById(SEARCH_ID)).thenReturn(true);
+
+        service.save(PERSISTED_HOTEL_SEARCH);
+
+        verify(repository).existsById(SEARCH_ID);
+        verify(repository, never()).save(any());
     }
 
 }
